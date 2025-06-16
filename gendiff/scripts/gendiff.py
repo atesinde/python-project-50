84361367c8f995ce273181dbc4fdc50d262c37dc
Anna S <ann.sapegina@gmail.com>
2025-06-16 20:14:40 +0300

    Добавление опции -format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,6 +4,7 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+parser.add_argument('-f', '-format', type=str, metavar='FORMAT', help='set format of output')
 parser.parse_args()
 
 def main():
