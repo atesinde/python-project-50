3e0b002b1485b8c67abf3cdf050a7b369a8c4a14
Anna S <ann.sapegina@gmail.com>
2025-06-25 18:44:41 +0300

    Исправление замечаний линтера в gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,8 +6,10 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('first_file')
 parser.add_argument('second_file')
-parser.add_argument('-f', '-format', type=str, metavar='FORMAT', help='set format of output')
+parser.add_argument('-f', '-format', type=str, metavar='FORMAT',
+                    help='set format of output')
 args = parser.parse_args()
+
 
 def generate_diff(filepath1, filepath2):
     with open(filepath1, 'r', encoding='utf-8') as file:
@@ -23,7 +25,8 @@ def generate_diff(filepath1, filepath2):
             if key in json_file1 and json_file2[key] == json_file1[key]:
                 diff_result = diff_result + f'    {key}: {json_file2[key]}\n'
             elif key in json_file1 and json_file2[key] != json_file1[key]:
-                diff_result = diff_result + f'  - {key}: {json_file1[key]}\n  + {key}: {json_file2[key]}\n'
+                diff_result = diff_result + f'  - {key}: {json_file1[key]}\n + \
+                {key}: {json_file2[key]}\n'
         if key not in json_file2:
             diff_result = diff_result + f'  - {key}: {json_file1[key]}\n'
         if key not in json_file1:
@@ -35,10 +38,12 @@ def generate_diff(filepath1, filepath2):
 
     return diff_result
 
+
 def main():
     print("Lets's generete some difference!")
     diff = generate_diff(args.first_file, args.second_file)
     print(diff)
+
 
 if __name__ == "__main__":
     main()
